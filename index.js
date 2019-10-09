ec3f90ebfca4b689fa0b3bbd7168c3ad50edb6e6
Marc BA <marc.bisbalarias@dowjones.com>
2019-10-09 07:56:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(kittens){
-const hey=kittens.appendKitten("Nasa");
-return hey;
+return kittens.appendKitten("Nasa");
 }
