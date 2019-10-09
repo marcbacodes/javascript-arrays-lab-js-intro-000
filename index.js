3f73d671ebe38d94697a96da2db8bfc26ba37383
Marc BA <marc.bisbalarias@dowjones.com>
2019-10-09 08:06:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function destructivelyAppendKitten(name){
 kittens.push(name)
 }
 
-function destructivelyPrependKitten(shift){
-  kittens.shift();
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
 }
